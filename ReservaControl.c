9e465af860ab39e4a51cb388efe8865faef1cda6
Tales Pereira Grechi <talespgrechi@gmail.com>
2021-01-05 20:01:49 -0300

    um pouco dos menus

---
+++ ReservaControl.c
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <stdio.h>
+
+
+void cancelarReservaControl(){
+    printf("DIGITE O COD DA ACOMODAÇÃO:");
+    // VALIDA
+    //SE DEU A VALIDAÇÃO:
+    //CHAMA O VISUALIZAR RESERVA, PRO CARA VE O QUE ELE VAI DELETAR
+    //cancelarReservaOficial();
+    //SE NAO DEU
+    printf("ERRO");
+}
